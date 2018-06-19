0db3a2bf4f5159a432422266c4dad7250e2e3d90
jlafayette <joshlafayette@gmail.com>
2018-06-18 21:37:47 -0700

    Clean up comments and print statements

--- cmd/kosync/main.go
+++ cmd/kosync/main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/jlafayette/kos-cli/deploy"
@@ -22,22 +21,10 @@ Options:
 `
 
 func main() {
-
-	// take three args, folder to watch, folder to sync to src, dest, filterstring (for ext)
-	// when watching, check filter, then
-	//		new -> copy
-	//      update -> update
-	//		delete -> delete
-	//		ignore others
-
 	filterPtr := flag.String("filter", "*.ks", "Glob string for files to be synced.")
 	fPtr := flag.String("f", "", "(Alias for filter) Glob string for files to be synced.")
 
 	flag.Parse()
-
-	fmt.Println("filter:", *filterPtr)
-	fmt.Println("     f:", *fPtr)
-	fmt.Println("  tail:", flag.Args())
 
 	if len(flag.Args()) != 2 {
 		// fmt.Fprintln(os.Stderr, "BAD!")
@@ -51,14 +38,12 @@ func main() {
 	}
 	src := flag.Args()[0]
 	dst := flag.Args()[1]
-	fmt.Println("filter:", filter)
-	fmt.Println("   src:", src)
-	fmt.Println("   dst:", dst)
 
 	// Initial sync
 	err := deploy.CopyFiles(src, dst, filter, false)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
 	}
 
 	watcher, err := fsnotify.NewWatcher()
@@ -73,21 +58,17 @@ func main() {
 		for {
 			select {
 			case event := <-watcher.Events:
-				// fmt.Printf("EVENT! %#v\n", event)
 				_, file := filepath.Split(event.Name)
 				m, _ := filepath.Match(filter, file)
 				if m {
 					switch event.Op {
 					case 1: // Create Op  = 1 << iota
-						// fmt.Printf("    Got %v event --> create empty\n", event.Op)
 						newFile, err := os.Create(replaceFirst(event.Name, src, dst))
 						if err != nil {
 							fmt.Printf("ERROR creating file: %v\n", err)
 						}
 						newFile.Close()
 					case 2: // Write --> copy
-						// fmt.Printf("    Got %v event --> copy\n", event.Op)
-						time.Sleep(time.Millisecond)
 						err := deploy.CopyFile(event.Name, replaceFirst(event.Name, src, dst), false)
 						if err != nil {
 							fmt.Printf("ERROR copying file: %v\n", err)
@@ -96,24 +77,18 @@ func main() {
 							// 	     because it is being used by another process.
 						}
 					case 4: // Remove --> remove
-						// fmt.Printf("    Got %v event --> remove\n", event.Op)
 						err := os.Remove(replaceFirst(event.Name, src, dst))
 						if err != nil {
 							fmt.Printf("ERROR removing file: %v\n", err)
 						}
 					case 8: // Rename --> remove
-						// fmt.Printf("    Got %v event --> remove\n", event.Op)
 						err := os.Remove(replaceFirst(event.Name, src, dst))
 						if err != nil {
 							fmt.Printf("ERROR removing file: %v\n", err)
 						}
 					case 16: // Chmod --> ignore
-						// fmt.Printf("    Got %v event --> ignore\n", event.Op)
-					default: // unknown
-						// fmt.Printf("    Got %v event --> ignore\n", event.Op)
+						// put something here if Chmod needs to be handled.
 					}
-				} else {
-					// fmt.Printf("    Name %s does not match filter %s\n", file, filter)
 				}
 			case err := <-watcher.Errors:
 				fmt.Println("WatcherError:", err)
